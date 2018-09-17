2c7b13ea1bd863749fdd8be2ef39889efa11b9d8
James Larsen <jlarsen@localhost.localdomain>
2018-09-16 20:45:16 -0400

    Added installation logic

--- install.sh
+++ install.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
 
+mkdir ~/.dotfiles/
+cp -r * ~/.dotfiles/
+pushd ~/.dotfiles/
+if [ -f ~/.bash_profile -a ! ~/.bash_profile.bck ]; then
+mv ~/.bash_profile ~/.bash_profile.bck
+fi
+ln -s ~/.dotfiles/bash/bash_profile.symlink ~/.bash_profile
+if [ -f ~/.bashrc -a ! ~/.bashrc.bck ]; then 
+mv ~/.bashrc ~/.bash_profile.bck
+fi
+ln -s ~/.dotfiles/bash/bashrc.symlink ~/.bashrc
+if [ -f ~/.vimrc -a ! ~/.vimrc.bck ]; then
+mv ~/.vimrc ~/.vimrc.bck
+fi
+ln -s ~/.dotfiles/vim/vimrc.symlink ~/.vimrc
+popd
